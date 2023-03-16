d16ee27f3709160599655ff3d8764e8e4c7f4e1d
aktan <kerimbekov1233@gmail.com>
2023-03-16 22:52:40 +0600

    testimonials

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -19,8 +19,8 @@ function HeroHome() {
           <svg className="max-w-full" width="564" height="552" viewBox="0 0 564 552" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
               <linearGradient id="illustration-02" x1="-3.766" y1="300.204" x2="284.352" y2="577.921" gradientUnits="userSpaceOnUse">
-                <stop stopColor="#5D5DFF" stopOpacity=".01" />
-                <stop offset="1" stopColor="#5D5DFF" stopOpacity=".32" />
+                <stop stopColor="#fff" stopOpacity=".01" />
+                <stop offset="1" stopColor="#fff" stopOpacity=".32" />
               </linearGradient>
             </defs>
             <path
